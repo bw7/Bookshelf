38164f591b528ef7054f2005ac06fcc943065911
Barry Wong <barrywong77@gmail.com>
2013-12-22 16:44:38 -0500

    Updated with app.get, app.put and app.post info

--- Bookshelf/lib/routes/index.js
+++ Bookshelf/lib/routes/index.js
@@ -7,8 +7,10 @@ module.exports = function(app) {
 
   /* For Books */
   app.get('/books/new', BookController.new);
+  app.get('/books/:id/edit', BookController.edit);
   app.get('/books', BookController.list);
   app.post('/books', BookController.create);
   app.get('/books/:id', BookController.show);
+  app.put('/books/:id', BookController.update);
   app.delete('books/:id', BookController.remove);
 }
